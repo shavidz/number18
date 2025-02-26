f0f31d4112b4ee488d77b14ab22f77190f0e5cc7
nini shavidze <shavidzenini141@gmail.com>
2025-02-26 18:45:55 +0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,9 @@
+// data.js
+
+const products = [
+    { title: "Laptop", price: 1200 },
+    { title: "Phone", price: 800 },
+    { title: "Tablet", price: 600 },
+    { title: "Monitor", price: 300 },
+    { title: "Mouse", price: 50 }
+];
